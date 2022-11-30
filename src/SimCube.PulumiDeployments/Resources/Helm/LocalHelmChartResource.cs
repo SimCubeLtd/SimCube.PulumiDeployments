657f5ef8c20b559f6e24ffdadcc7b403d46fca8d
Prom3theu5 <dave.sekula@gmail.com>
2022-11-30 14:02:59 +0000

    Add local helm chart option

---
+++ src/SimCube.PulumiDeployments/Resources/Helm/LocalHelmChartResource.cs
@@ -0,0 +1,35 @@
+﻿using Pulumi.Kubernetes.Helm.V3;
+
+namespace SimCube.PulumiDeployments.Resources.Helm;
+
+public abstract class LocalHelmChartResource : BaseHelmChartResource
+{
+    protected LocalHelmChartResource(NamespaceResource @namespace, string type, string name, ComponentResourceOptions? options = null)
+        : base(@namespace, type, name, options)
+    {
+
+    }
+
+    protected Release CreateRelease(
+        NamespaceResource @namespace,
+        string chartName,
+        string pathToChart,
+        string? helmChartVersion = null,
+        List<FileAsset>? helmValuesFiles = null,
+        bool waitForJobs = true,
+        bool skipAwait = false,
+        int timeout = 1200)
+          =>  new(
+              chartName,
+              new()
+              {
+                  Namespace = @namespace.NamespaceName,
+                  Chart = pathToChart,
+                  Version = helmChartVersion ?? string.Empty,
+                  WaitForJobs = waitForJobs,
+                  SkipAwait = skipAwait,
+                  Timeout = timeout,
+                  ValueYamlFiles = new List<AssetOrArchive>(helmValuesFiles ?? new List<FileAsset>()),
+              },
+              CustomResourceOptions);
+}
